ea6c15263b99f77771616d73a38f137e655827f7
ibrahim Yıldız <55101344+ibrahimyldz11q@users.noreply.github.com>
2020-08-29 11:47:58 +0300

    Add files via upload

---
+++ commandline.sql
@@ -0,0 +1,43 @@
+Postgresql Genel Komut Satırı Kodları
+
+Cmd Ekranında Postgresql Baglanmak Icın Asagidaki Postgresql Dizinlerine Gitmeniz Lazim Sizlerde Kurulum Yeri
+Farklı Olabilir cd deyiz dosya dizinin içine Giriyoruz
+
+cd C:\Program Files\PostgreSQL\12\bin
+psql -U postgres -D Herhangi Db Adi veya -h host 
+Password = ** 
+
+\l Veritabanlarini Listeler
+
+\c dbadi Veritabanina Baglanir
+
+\dt Tabloları Listeler
+
+\d tablo_ismi Tablonun Ozelliklerini Gormek Icın
+
+\dn Semalari Listeler
+
+\dv Viewları Listeler
+
+\du Kullanicilari Listeler
+
+\g Bir Onceki Komutu Tekrar Getirir
+
+\s Komut Gecmişi Gösterir
+
+\? Tum Komtları Gosterir
+
+\H FETCH fetch Ile Bilgi Verir
+
+\timing Sorgu Hızını Gosterir
+
+\e Not Defterinden SQL Yazmanıza Yarar
+
+\ef Not Defterinden Function Calistirmaniza Yarar
+
+\q Cıkıs Yapar
+
+
+
+
+
